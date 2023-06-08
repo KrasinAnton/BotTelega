177ed2d1b0a34d95ab77cb58f64272cfd518a458
KrasinAnton <2saper@mail.ru>
2023-06-09 02:43:08 +0500

    update

--- src/main/Java/Bot.java
+++ src/main/Java/Bot.java
@@ -1,3 +1,4 @@
+import com.cloudimpl.outstack.app.App;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
@@ -11,15 +12,30 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class Bot extends TelegramLongPollingBot {
+
     final private String BOT_NAME = "@smartcitatybot";
     Storage storage;
 
-    Bot() {
+    Bot() throws IOException {
         storage = new Storage();
     }
 
@@ -30,10 +46,9 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public String getBotToken() {
-        //создаем две константы, присваиваем им значения токена и имя бота соответсвтенно
-        String BOT_TOKEN = "6264974943:AAH4Oi6CJ23RPKT6GpWk5iHCgCFXmbhBt38";
-        return BOT_TOKEN;
+        return loadTokenFromProperties();
     }
+
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasCallbackQuery()) {
@@ -81,7 +96,6 @@ public class Bot extends TelegramLongPollingBot {
             response = storage.getRandQuote();
         else if (textMsg.equals("Просвяти"))
             response = storage.getRandQuote();
-
         else
             response = "Сообщение не распознано";
 
@@ -96,9 +110,7 @@ public class Bot extends TelegramLongPollingBot {
         button1.setCallbackData("/enlighten");
 
         List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
-
         keyboardButtonsRow1.add(button1);
-
 
         List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
         rowList.add(keyboardButtonsRow1);
@@ -106,117 +118,22 @@ public class Bot extends TelegramLongPollingBot {
         inlineKeyboardMarkup.setKeyboard(rowList);
         return inlineKeyboardMarkup;
     }
+
+    private String loadTokenFromProperties() {
+        Properties prop = new Properties();
+        try {
+            // Загружаем файл свойств из classpath
+            prop.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+
+            // Получаем значение свойства "token"
+            return prop.getProperty("token");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
 
 
-
-    /*@Override
-    public void onUpdateReceived(Update update) {
-        if (update.hasCallbackQuery()) {
-            CallbackQuery callbackQuery = update.getCallbackQuery();
-            // Получение данных обратного вызова (callback data)
-            String callbackData = callbackQuery.getData();
-            // Получение идентификатора чата
-            long chatId = callbackQuery.getMessage().getChatId();
-
-            // Ваша обработка обратного вызова на основе callbackData
-            // Например, отправка ответа в чат
-            try {
-                execute(new SendMessage().setChatId(chatId).setText("Callback Data: " + callbackData));
-
-
-        try {
-            if (update.hasMessage() && update.getMessage().hasText()) {
-                //Извлекаем из объекта сообщение пользователя
-                Message inMess = update.getMessage();
-                //Достаем из inMess id чата пользователя
-                String chatId = inMess.getChatId().toString();
-                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
-                String response = parseMessage(inMess.getText());
-
-
-
-
-                // Создаем клавиатуру
-                /*ReplyKeyboardMarkup replyKeyboardMarkup = new
-                        ReplyKeyboardMarkup();
-                replyKeyboardMarkup.setSelective(true);
-                replyKeyboardMarkup.setResizeKeyboard(true);
-                replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-                // Создаем список строк клавиатуры
-                List<KeyboardRow> keyboard = new ArrayList<>();
-
-                // Первая строчка клавиатуры
-                KeyboardRow keyboardFirstRow = new KeyboardRow();
-                // Добавляем кнопки в первую строчку клавиатуры
-                keyboardFirstRow.add("▶Старт");
-                keyboardFirstRow.add("\uD83D\uDD04Просвяти");
-
-                // Вторая строчка клавиатуры
-                KeyboardRow keyboardSecondRow = new KeyboardRow();
-                // Добавляем кнопки во вторую строчку клавиатуры
-                keyboardSecondRow.add("citatnica.ru");
-                keyboardSecondRow.add("Инфо");
-
-                // Добавляем все строчки клавиатуры в список
-                keyboard.add(keyboardFirstRow);
-                keyboard.add(keyboardSecondRow);
-                // и устанавливаем этот список нашей клавиатуре
-                replyKeyboardMarkup.setKeyboard(keyboard);*/
-
-               /* InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-                InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
-                inlineKeyboardButton.setText("Просвяти");
-                inlineKeyboardButton.setCallbackData("Button Просвяти has been pressed");
-                List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
-                keyboardButtonsRow.add(inlineKeyboardButton);
-                //keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Fi4a").CallbackData("CallFi4a"));
-                List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
-                rowList.add(keyboardButtonsRow);
-                inlineKeyboardMarkup.setKeyboard(rowList);
-                //Создаем объект класса SendMessage - наш будущий ответ пользователю
-                SendMessage outMess = new SendMessage();
-
-
-
-
-                //Добавляем в наше сообщение id чата а также наш ответ
-                outMess.setChatId(chatId);
-                outMess.setText(response);
-                outMess.setReplyMarkup(inlineKeyboardMarkup);
-                //outMess.setReplyMarkup(replyKeyboardMarkup);
-
-                //Отправка в чат
-                execute(outMess);
-
-            }
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public String parseMessage(String textMsg) {
-        String response;
-
-        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
-        if (textMsg.equals("/start"))
-            response = "Приветствую, бот знает много цитат. Жми Просвяти, чтобы получить случайную из них";
-        else if (textMsg.equals("/enlighten"))
-            response = storage.getRandQuote();
-        else if(textMsg.equals("/start") || textMsg.equals("▶Старт"))
-            response = "Приветствую, бот знает много цитат. Жми \uD83D\uDC49 Просвяти, чтобы получить случайную из них";
-        else if(textMsg.equals("/enlighten") || textMsg.equals("\uD83D\uDD04Просвяти"))
-            response = storage.getRandQuote();
-        else if (textMsg.equals("citatnica.ru"))
-            response = "https://citatnica.ru/";
-        else
-            response = "Сообщение не распознано";
-
-        return response;
-    }
-
-    }*/
 
 
--- src/main/Java/Main.java
+++ src/main/Java/Main.java
@@ -2,6 +2,8 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+
 
 public class Main {
     public static void main(String args[])
@@ -11,6 +13,8 @@ public class Main {
             telegramBotsApi.registerBot(new Bot());
         } catch (TelegramApiException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
